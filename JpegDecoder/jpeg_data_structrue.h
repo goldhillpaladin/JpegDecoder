db0b4c6a578b9586111f29be9821b4fded504806
goldhillpaladin <qiansheng870601@163.com>
2018-02-11 17:06:58 +0800

    增加参数的注释。

--- JpegDecoder/jpeg_data_structrue.h
+++ JpegDecoder/jpeg_data_structrue.h
@@ -24,45 +24,45 @@ struct APPn {
 struct FrameHeader {
     FrameHeader() : Lf(0), P(0), Y(0), X(0), Nf(0) {}
 
-    // Frame component-specification parameters
+    // Frame component-specification parameters，用于指定每个 component 的参数。
     struct ComponentParam {
         ComponentParam() : Ci(0), Hi(0), Vi(0), Tqi(0) {}
-        uint8_t Ci;  // Component identifier
-        uint8_t Hi;  // Horizontal sampling factor
-        uint8_t Vi;  // Vertical sampling factor
-        uint8_t Tqi; // Quantization table destination selector
+        uint8_t Ci;  // component id。Component identifier.
+        uint8_t Hi;  // 水平采样系数(1-4)。Horizontal sampling factor
+        uint8_t Vi;  // 垂直采样系数(1-4)。Vertical sampling factor
+        uint8_t Tqi; // 对应的量化表的 id。Quantization table destination selector
     };
 
     uint16_t Lf; // Frame header length
-    uint8_t P;   // Sample precision
-    uint16_t Y;  // Number of lines
-    uint16_t X;  // Number of samples per line
-    uint8_t Nf;  // Number of image components in frame
+    uint8_t P;   // Sample precision。Baseline Sequential DCT 模式下，值为 8
+    uint16_t Y;  // Number of lines。垂直方向的像素点的个数
+    uint16_t X;  // Number of samples per line。水平方向的像素点的个数。
+    uint8_t Nf;  // Number of image components in frame。frame 中 component 的个数。
     std::vector<ComponentParam> com_params; // Frame component-specification parameters
 };
 
 // SOS: scan header
 struct ScanHeader {
     struct ComponentParam {
-        uint8_t Csj; // Scan component selector
-        uint8_t Tdj; // DC entropy coding table destination selector
-        uint8_t Taj; // AC entropy coding table destination selector
+        uint8_t Csj; // Scan component selector。与 FrameHeader::ComponentParam 的 Ci 对应，用于找到对应的 FrameHeader::ComponentParam
+        uint8_t Tdj; // DC entropy coding table destination selector。对应 HuffmanTable::Th。
+        uint8_t Taj; // AC entropy coding table destination selector。对应 HuffmanTable::Th。
     };
 
     uint16_t Ls; // Scan header length
     uint8_t Ns;  // Number of image components in scan
     std::vector<ComponentParam> com_params; // Scan component-specification parameters
-    uint8_t Ss;  // Start of spectral or predictor selection
-    uint8_t Se;  // End of spectral selection
-    uint8_t Ah;  // Successive approximation bit position high
-    uint8_t Al;  // Successive approximation bit position low or point transform
+    uint8_t Ss;  // Start of spectral or predictor selection。Baseline Sequential DCT 模式下为 0
+    uint8_t Se;  // End of spectral selection。Baseline Sequential DCT 模式下为 63
+    uint8_t Ah;  // Successive approximation bit position high。Baseline Sequential DCT 模式下为 0
+    uint8_t Al;  // Successive approximation bit position low or point transform。Baseline Sequential DCT 模式下为 0
 };
 
 // DQT: Define quantization table(s)
 struct DefQuantTable {
     struct QuantTable {
         uint8_t Pq;
-        uint8_t Tq;
+        uint8_t Tq; // 量化表 id，与 FrameHeader::ComponentParam::Tqi 对应
         std::array<int16_t, 64 > Qk;
     };
 
@@ -74,6 +74,7 @@ struct DefQuantTable {
 struct DefHuffmanTable {
     struct HuffmanTable {
         uint8_t Tc;                                // Table class (0: DC; 1: AC)
+        // 对应 ScanHeader::ComponentParam 的 Tdj，Taj
         uint8_t Th;                                // Huffman table destination identifier (0: Y; 1: C)
         std::array<uint8_t, 16> Li;                // Number of Huffman codes of length i
         std::array<std::vector<uint8_t>, 16> Vij;  // Value associated with each Huffman code
